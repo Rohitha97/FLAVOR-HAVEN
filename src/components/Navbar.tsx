5e31601ed91edb9454959d13925743b28922e9b8
Rohitha Rathnayake <rohith_rathnayake@yahoo.com>
2023-09-28 21:18:10 +0530

    Nav bar changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,69 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Menu from "./Menu";
 import Link from "next/link";
 import CartIcon from "./CartIcon";
-import Image from "next/image";
-import UserLinks from "./UserLinks";
-import { useSession } from "next-auth/react";
-import AdminLinks from "./AdminLinks";
+import { signOut, useSession } from "next-auth/react";
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+  activeLink: string;
+  setActiveLink: (href: string) => void;
+}
+
+const NavLink = ({ href, children, activeLink, setActiveLink }: NavLinkProps) => {
+  return (
+    <Link href={href}>
+      <div onClick={() => setActiveLink(href)} className={`border-2 ${activeLink === href ? "border-red-500" : "border-transparent"} hover:border-red-500 rounded-md p-2 focus:outline-none`}>
+        {children}
+      </div>
+    </Link>
+  );
+};
 
 const Navbar = () => {
-  const user = false;
+  const { data: session, status } = useSession();
+  const [activeLink, setActiveLink] = useState("/");
+
   return (
     <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
-      {/* LEFT LINKS */}
       <div className="hidden md:flex gap-4 flex-1">
-        <Link href="/">Homepage</Link>
-        <Link href="/menu">Menu</Link>
-        <Link href="/contact">Contact</Link>
+        <NavLink href="/" activeLink={activeLink} setActiveLink={setActiveLink}>
+          Homepage
+        </NavLink>
+        <NavLink href="/menu" activeLink={activeLink} setActiveLink={setActiveLink}>
+          Menu
+        </NavLink>
+        <NavLink href="/contact" activeLink={activeLink} setActiveLink={setActiveLink}>
+          Contact
+        </NavLink>
       </div>
-      {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">Flavor Haven</Link>
       </div>
-      {/* MOBILE MENU */}
       <div className="md:hidden">
         <Menu />
       </div>
-      {/* RIGHT LINKS */}
       <div className="hidden md:flex gap-4 items-center justify-end flex-1">
         <CartIcon />
-        <AdminLinks />
-        <UserLinks />
+        {status === "authenticated" && session?.user.isAdmin && (
+          <NavLink href="/settings" activeLink={activeLink} setActiveLink={setActiveLink}>
+            Settings
+          </NavLink>
+        )}
+        {status === "authenticated" ? (
+          <>
+            <NavLink href="/orders" activeLink={activeLink} setActiveLink={setActiveLink}>
+              Orders
+            </NavLink>
+            <span className="ml-4 cursor-pointer hover:border-red-500 rounded-md p-2 focus:outline-none" onClick={() => signOut()}>
+              Logout
+            </span>
+          </>
+        ) : (
+          <Link href="/login">Login</Link>
+        )}
       </div>
     </div>
   );
--- src/components/UserLinks.tsx
+++ src/components/UserLinks.tsx
@@ -2,15 +2,27 @@
 
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useState } from "react";
 
 const UserLinks = () => {
   const { status } = useSession();
+  const [activeLink, setActiveLink] = useState("/");
+
   return (
     <div>
       {status === "authenticated" ? (
         <div>
-          <Link href="/orders">Orders</Link>
-          <span className="ml-4 cursor-pointer" onClick={() => signOut()}>Logout</span>
+          <Link href="/orders">
+            <div
+              onClick={() => setActiveLink("/orders")}
+              className={`border-2 ${activeLink === "/orders" ? "border-red-500" : "border-transparent"} hover:border-red-500 rounded-md p-2 focus:outline-none`}
+            >
+              Orders
+            </div>
+          </Link>
+          <span className="ml-4 cursor-pointer hover:border-red-500 rounded-md p-2 focus:outline-none" onClick={() => signOut()}>
+            Logout
+          </span>
         </div>
       ) : (
         <Link href="/login">Login</Link>
